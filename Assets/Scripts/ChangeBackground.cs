97a78c10cbab811eee94b84394d01cadddcd768d
chj259 <chjessie@seas.upenn.edu>
2025-11-06 00:38:14 -0500

    Add fade in and start refactoring dialogue

---
+++ Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+public class ChangeBackground : MonoBehaviour
+{
+    private UnityEngine.UI.Image background_img;
+    private float transition;
+    private int fade_rate = 100;
+
+    void Start()
+    {
+        background_img = this.GetComponent<UnityEngine.UI.Image>();
+    }
+
+    public IEnumerator FadeOut()
+    {
+        transition = 0;
+        yield return StartCoroutine(DoFadeOut());
+
+    }
+    
+    public IEnumerator FadeIn()
+    {
+        transition = 0;
+        yield return StartCoroutine(DoFadeIn());
+    }
+    IEnumerator DoFadeOut()
+    {
+        while (transition < 0.01)
+        {
+            //Debug.Log(transition);
+            transition += Time.deltaTime / fade_rate;
+            background_img.color = Color.Lerp(background_img.color, Color.black, transition);
+            yield return null;
+        }
+
+        yield return null;
+    }
+    
+    IEnumerator DoFadeIn()
+    {
+        while (transition < 0.01)
+        {
+            transition += Time.deltaTime/fade_rate;
+            background_img.color = Color.Lerp(background_img.color, Color.white, transition);
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
